4e54492c1bf878b29c61d17d67d11771e5ccc3a6
NadiaElmou <elmoudennadia8@gmail.com>
2023-02-23 20:45:35 +0100

    rendre la page grande

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ class App(tk.Tk):
         super().__init__()
        # last_time = last_time_visited()
         # Setting up Initial parameters
-
-
         # Setting up Initial parameters
         self.title(f"Easylearn")
         #self.geometry("800x600")
